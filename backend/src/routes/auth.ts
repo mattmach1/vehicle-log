244931a89d12b4d7d07eeab4432697bf7c9e212c
Matt <mbrown2595@gmail.com>
2026-04-04 13:31:38 -0500

    create frontend

---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,66 @@
+import { Router } from "express";
+import type { Request, Response } from "express";
+import { prisma } from "../lib/prisma.js";
+import argon2 from "argon2";
+
+const router = Router();
+
+router.post("/register", async (req: Request, res: Response) => {
+    try {
+        const { email, password, name } = req.body;
+
+        const existingUser = await prisma.user.findUnique({
+            where: { email }
+        });
+
+        if (existingUser) {
+            return res.status(400).json({ error: "Email already in use" });
+        }
+
+        const hashedPassword = await argon2.hash(password);
+
+        const user = await prisma.user.create({
+            data: { email, password: hashedPassword, name },
+            select: {
+                id: true,
+                email: true,
+                name: true
+            }
+        });
+
+        return res.status(201).json(user);
+
+    } catch (err) {
+        return res.status(500).json({ error: "Server error" });
+    }
+})
+
+router.post("/login", async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
+
+        if (!user) {
+            return res.status(401).json({ error: "email not found" });
+        }
+
+        const valid = await argon2.verify(user.password, password);
+        if (!valid) {
+            return res.status(401).json({ error: "Invalid password" });
+        }
+
+        return res.status(200).json({
+            id: user.id,
+            email: user.email,
+            name: user.name
+        })
+    } catch (err) {
+        return res.status(500).json({ error: "Server error" });
+    }
+});
+
+export default router
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import api from "../api/axios";
+
+
+function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    async function handleSubmit(e: React.SubmitEvent) {
+        e.preventDefault()
+        try {
+            const res = await api.post("/auth/login", { email, password })
+            console.log(res.data)
+        } catch (error) {
+            setError("Invalid credentials")
+        }
+    }
+
+    return (
+        <div>
+            <h1>Login</h1>
+            {error && <p>{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
